46266cd76e185dc5182aba6875f63b46f88cd06d
PavanXworkz <pavanbs.xwokz@gmail.com>
2023-06-02 12:49:19 +0530

    test1_june

---
+++ Test .java
@@ -0,0 +1,113 @@
+1)Explain java and javac
+   javac is a command which invokes java compiler to compile the source code.
+			it converts source code to bytete code.
+			This happens in JDK.
+   java is a command which invokes java runner  to run the byte  code.
+        This hapens in JRE.
+
+2)  Explain JDK,JRE and JVM.
+	JDK
+		Java development kit
+		It consist all the set of libraries required for java program.
+		It consist JRE which contains JVM.
+		It converts Source code to byte code.
+		It debugs the program.
+		
+	JRE	
+		Java RunTime Environament
+		It runs byte code.
+		It activate during running the program
+		
+	JVM
+		Java Vertual Machine
+	    Phically absent present vetualy.
+		
+3)List the limations of array.
+	Array has it's own limation like
+	where it cannot store different data types but can store only the same data type.
+	Once initialised cannot be increased or decreased.
+	
+	
+4)Explain class, object and signature of class.
+	class is atemplet of object.
+	object is instance of a class.
+	we cannot declare object with out a class.
+	where class contains address of object.
+	it contain the memory of the class
+	it is depindent on class so it is called instance to class
+	
+	class ClassName(){
+	
+	
+	
+	}
+	
+5)Difference between .java and .class file
+		.java
+		it is a source code which is written in english and can understand by humans 
+		it is a format where source code is saved
+		.class 
+		it is byte code where compiler converts it during compilation period
+		it is saved in .class formate.
+
+6)Explain method signatire.
+ 
+ public static void main(){
+ 
+		method body
+ 
+ }
+  method consist:- acces specifier non-access specifier return type method name()
+  
+  
+ 7)whate is java?
+	java is a high end programming language
+	it is top down approch programming language
+	it is secured language.
+	it is easy to understand 
+	
+8)list the java IDE's
+	note pad 
+	edit+
+	eclips
+	intelij idea
+	note pad++
+ 
+9)Whate is variables ?types
+	it is a container which holds the value of valueof the data type.
+		types
+		local variable 		
+		declared with in the method 
+		it is method level variable
+		
+		static variable 
+		declared outside the method  inside the class
+		declared using static keyword
+		
+		instance variable
+		declared inside the class 
+		declared duringobject creation
+ 		parameter variable
+		declared as a parameter of method
+		
+10)		List 15 keyword
+        float  int  double char long boolean return break public void private protected class 
+
+		
+11) class variable?
+
+12)Data types and types
+    it is container holds the value of variable
+types 
+primitive 
+it contains 8 types
+int flot bouble long char booolean
+non-primitive
+String and classname
+
+13)what is method overloding?
+   using same method with difference in parameter or length of parameter	
+		
+14)dot operator
+it access method from other class
+ 
